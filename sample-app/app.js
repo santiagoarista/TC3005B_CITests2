30422d09312dc6852e1d551f8529b294ac384560
santiagoarista <santiagoaristav@outlook.com>
2026-04-08 21:20:45 -0600

    feat: initialize sample app with Express and Jest
    - Add package.json with project metadata, dependencies, and scripts
    - Create server.js to set up the Express server listening on a specified port

---
+++ sample-app/app.js
@@ -0,0 +1,14 @@
+const express = require("express")
+
+const app = express();
+//app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+})
+
+app.get('/name/:name', (req, res) => {
+    res.send(`Hello ${req.params.name}`)
+})
+
+module.exports = app
